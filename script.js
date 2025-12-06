6d9bffbf16c4b165c121bd57cbc575df1d05b482
Krzysztof PC <olej1155@gmail.com>
2025-12-06 20:30:34 +0100

    add delete buttons functionality

--- script.js
+++ script.js
@@ -9,33 +9,8 @@ input.addEventListener("keydown", function(event) {
     }
 });
 
-// toggle completed - uncompleted todos
-todos.addEventListener("click", function(event) {
-    event.target.classList.toggle("completed");
-
-    getTrash();
-
-});
-
-function getTrash() {
-
-    const li = document.getElementsByTagName("li");
-    const deleteButton = document.querySelectorAll(".delete-button");
-
-    for (let i = 0; i < li.length; i++) {
-        if (li[i].classList[0] === "completed") {
-            deleteButton[i].style.visibility = "visible";
-        } else {
-            deleteButton[i].style.visibility = "hidden";
-        }
-    }
-    console.log(li);
-    console.log(deleteButton);
-}
 
 button.addEventListener("click", function (event) { addToDo(); });
-
-
 
 // adding new to-do and delete button
 function addToDo() {
@@ -45,6 +20,7 @@ function addToDo() {
     if (todoText !== "") {
         let todoEl = document.createElement("li"); // utworzony w pamięci
         let deleteButton = document.createElement("button");
+        deleteButton.type = "button";
         deleteButton.classList.add("delete-button");
         deleteButton.innerHTML = "<img src=\"trash.png\" alt=\"delete\" class=\"trash\">";
         todoEl.innerHTML = todoText;
@@ -53,3 +29,36 @@ function addToDo() {
         input.value = "";
     }
 }
+
+// toggle completed - uncompleted todos
+todos.addEventListener("click", function(event) {
+    event.target.classList.toggle("completed");
+
+    getTrash();
+
+});
+
+// getting delete button if completed
+function getTrash() {
+
+    let li = document.getElementsByTagName("li");
+    let deleteButton = document.querySelectorAll(".delete-button");
+
+    for (let i = 0; i < li.length; i++) {
+        if (li[i].classList[0] === "completed") {
+            deleteButton[i].style.visibility = "visible";
+        } else {
+            deleteButton[i].style.visibility = "hidden";
+        }
+    }
+}
+
+todos.addEventListener("click", function(event) {
+    // bo img jest dzieckiem deleteButtona, klikajac w img kosza klikasz target to deleteButton
+    if(event.target.closest(".delete-button")) {
+        const button = event.target.closest(".delete-button");
+        const li = button.previousElementSibling; // element poprzedni (wyżej)
+        li.remove();
+        button.remove();
+    }
+})
