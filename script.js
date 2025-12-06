d9ebeae7e9f9252bb57a9e1fc4e3a9ff8cff8fda
Krzysztof PC <olej1155@gmail.com>
2025-12-06 17:22:09 +0100

    add delete buttons wip

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.getElementById("input");
-const button = document.getElementById("button");
+const button = document.getElementById("input-button");
 const todos = document.getElementById("todos");
+
 
 // block enter in input
 input.addEventListener("keydown", function(event) {
@@ -11,20 +12,26 @@ input.addEventListener("keydown", function(event) {
 
 // toggle completed - uncompleted todos
 todos.addEventListener("click", function(event) {
-
     event.target.classList.toggle("completed");
-})
+});
 
 button.addEventListener("click", function (event) { addToDo(); });
 
+
+
+// adding new to-do and delete button
 function addToDo() {
 
     let todoText = input.value;
 
     if (todoText !== "") {
         let todoEl = document.createElement("li"); // utworzony w pamięci
+        let deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-button");
+        deleteButton.innerHTML = "<img src=\"trash.png\" alt=\"delete\" class=\"trash\">";
         todoEl.innerHTML = todoText;
         todos.appendChild(todoEl);
+        todos.appendChild(deleteButton);
         input.value = "";
     }
 }
